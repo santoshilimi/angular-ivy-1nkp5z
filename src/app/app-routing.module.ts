7b7cb03acfae527db415a9d1f2f8bb0102f392b1
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-01-13 15:45:20 +0530

    Changes in files and added comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EpubComponent } from './epub/epub.component';
-import { VideoComponent } from './video/video.component';
 import { PdfComponent } from './pdf/pdf.component';
 
 const routes: Routes = [
-  {path: 'epub', component: EpubComponent},
-  {path: 'video', component: VideoComponent},
   {path: 'pdf', component: PdfComponent}
 ];
 
